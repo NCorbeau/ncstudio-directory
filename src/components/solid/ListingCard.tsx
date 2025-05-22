6ff0775a3f977f4a36c2fc0d86ee1fc7dcf6b5ce
NCorbeau <glownia@gmail.com>
2025-05-22 21:31:44 +0200

    Fix

--- src/components/solid/ListingCard.tsx
+++ src/components/solid/ListingCard.tsx
@@ -152,7 +152,7 @@ export default function ListingCard(props: ListingCardProps) {
             <p class="card-description">{listing.description}</p>
             
             <div class="card-footer">
-              <Show when={listing..fieldsaddress}>
+              <Show when={listing.fields.address}>
                 <div class="card-address">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                     <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
